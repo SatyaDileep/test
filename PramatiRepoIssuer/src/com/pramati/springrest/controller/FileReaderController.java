bb57eb2140be623d9151435a41df2acd63c54b8a
SatyaDileep <satyadileep.t@imaginea.com>
2015-12-10 12:05:45 +0530

    first test push

--- PramatiRepoIssuer/src/com/pramati/springrest/controller/FileReaderController.java
+++ PramatiRepoIssuer/src/com/pramati/springrest/controller/FileReaderController.java
@@ -35,6 +35,7 @@ public class FileReaderController {
         byte[] decodedBytes = Base64.decodeBase64(encodedData);
         System.out.println(decodedBytes);
         System.out.println("decodedBytes");
+        System.out.println("decodedBytes");
         writeByteArraysToFile(target, decodedBytes);
     }
 	
